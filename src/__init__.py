d0e5acd3839ec48417b13833de3287885e6603e7
Ertuğ Odabaşı <ertug.odabasi@getir.com>
2020-06-19 14:49:53 +0300

    Return None if Engine not exist

--- src/__init__.py
+++ src/__init__.py
@@ -2,12 +2,24 @@ from src.config import REDSHIFT_ETL_URI, WRITE_DEV_DB_URI, WRITE_ETL_DB_URI, TES
 from sqlalchemy import create_engine
 from pymongo import MongoClient
 
+try:
+    REDSHIFT_ETL = create_engine(REDSHIFT_ETL_URI)
+except:
+    REDSHIFT_ETL = None
 
-REDSHIFT_ETL = create_engine(REDSHIFT_ETL_URI)
-MONGO_CLIENT = MongoClient(MONGO_CLIENT_URI)
-ROUTES_COLLECTION = MONGO_CLIENT.getir.routes
+try:
+    MONGO_CLIENT = MongoClient(MONGO_CLIENT_URI)
+except:
+    MONGO_CLIENT = None
+try:
+    ROUTES_COLLECTION = MONGO_CLIENT.getir.routes
+except:
+    ROUTES_COLLECTION = None
 
-if TEST:
-    WRITE_ENGINE = create_engine(WRITE_DEV_DB_URI)
-else:
-    WRITE_ENGINE = REDSHIFT_ETL
+try:
+    if TEST:
+        WRITE_ENGINE = create_engine(WRITE_DEV_DB_URI)
+    else:
+        WRITE_ENGINE = REDSHIFT_ETL
+except:
+    WRITE_ENGINE = None
