0750fab79568bee9ecef400c47fd0de8acf5f6c9
ebruyarencatak <ebru.catak@getir.com>
2022-02-03 17:23:33 +0300

    Artisan and Food routes in MongoDB were encoded. These routes decoded with polyline.

--- src/Route.py
+++ src/Route.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from bson import ObjectId
+import polyline
 import pickle
 
 
@@ -18,16 +19,20 @@ class Route:
         all_datas = []
 
         for data in cursor:
-            for i, location in enumerate(data['route']):
-                temp_dict = {}
-                temp_dict['route_id'] = str(data['_id'])
-                temp_dict['index'] = i
-                temp_dict['lon'] = location['coordinates'][0]
-                temp_dict['lat'] = location['coordinates'][1]
-                temp_dict['time'] = location['time']
-                temp_dict['acc'] = location['acc']
-                all_datas.append(temp_dict)
-
+            if 'route' in data:
+                for i, location in enumerate(data['route']):
+                        temp_dict = {}
+                        temp_dict['route_id'] = str(data['_id'])
+                        temp_dict['index'] = i
+                        temp_dict['lon'] = location['coordinates'][0]
+                        temp_dict['lat'] = location['coordinates'][1]
+                        temp_dict['time'] = location['time']
+                        temp_dict['acc'] = location['acc']
+                        all_datas.append(temp_dict)
+            else:
+                routes_data = decode(data)
+                for dict in routes_data:
+                    all_datas.append(dict)
         return pd.DataFrame(all_datas)
 
     def __get_routes_from_temp_table(self):
@@ -63,3 +68,31 @@ class Route:
             data = self._convert_cursor_to_routes_df(cursor)
 
         return data
+
+# decoding encoded route data
+def decode(data):
+    filtered_data = {key: data[key] for key in data.keys() & {'encoded', 'times'}}
+    df = pd.DataFrame([filtered_data])
+    route_id = str(data.get('_id'))
+    start_time = data.get('startTime')
+    df['decoded_routes'] = df.encoded.apply(polyline.decode, precision=7)
+    df['timeList'] = df.times.apply(lambda x: [int(i, 36) for i in x.split(";") if i != ''])
+
+    lat_lons = df.apply(lambda x: pd.DataFrame({
+            'route_id': route_id,
+            'time': [start_time + datetime.timedelta(seconds=t / 20) for t in x['timeList']],
+            'lat': [r[0] for r in x['decoded_routes']],
+            'lon': [r[1] for r in x['decoded_routes']],
+        }), axis=1)
+    rows = []
+    # appending rows
+    for self in lat_lons:
+        if len(self) > 0:
+            route_id = self['route_id'][0]
+            for i in self.index:
+                if route_id is not None and self['time'][i] is not None and self['lat'][i] is not None and \
+                        self['lon'][i] is not None:
+                    dict = {'route_id': route_id, 'time': self['time'][i], 'lat': self['lat'][i],
+                            'lon': self['lon'][i]}
+                    rows.append(dict)
+    return rows
