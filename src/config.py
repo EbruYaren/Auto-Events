90f5653b972c81bc69711f6726752a040abcdd79
ebruyarencatak <ebru.catak@getir.com>
2023-06-20 11:09:48 +0300

    fix try

--- src/config.py
+++ src/config.py
@@ -379,10 +379,10 @@ CREATE TABLE if not exists {schema}.delivery_date_prediction
 
 RUN_INTERVAL = timedelta(hours=1, minutes=30)
 
-DOMAIN_LIST = ['depart', 'depart_new',  'reach', 'depart,reach', 'depart_from_client', 'depart,reach,depart_from_client',
+DOMAIN_LIST = ['depart', 'depart_new', 'reach', 'depart,reach', 'depart_from_client', 'depart,reach,depart_from_client',
                'reach_to_merchant', 'depart_from_merchant', 'deliver', 'depart_from_courier_warehouse',
                'depart,reach,depart_from_client,reach_to_merchant',
-               'depart,reach,depart_from_client,reach_to_merchant,deliver,depart_from_merchant,depart_from_courier_warehouse']
+               'depart,depart_new,reach,depart_from_client,reach_to_merchant,deliver,depart_from_merchant,depart_from_courier_warehouse']
 DEFAULT_DOMAIN = 'depart,depart_new,reach,depart_from_client,reach_to_merchant,deliver,depart_from_merchant,depart_from_courier_warehouse'
 
 DEFAULT_TYPE = 'HOURLY'
